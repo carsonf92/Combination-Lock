b59d345a4ef8a2879b771121e8246e92f3b168bd
Carson Ford <carsonf92@gmail.com>
2019-02-25 20:25:01 -0500

    build

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/* scripts.js */
+
+// ***
+// Combination Lock
+// ***
+
+var combinationLock = {
+	combination: 
+	indicator: false,
+	wheels: [0, 0, 0, 0],
+	check: function() {
+		// check if the wheels match the combination
+	},
+	spin: function() {
+		// randomize the wheels
+	}
+}
+
+
+// ***
+// Presentation
+// ***
+
