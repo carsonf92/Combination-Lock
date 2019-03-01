01f0499ea54ab8ee4b490740653f2354bc1afd2c
Carson Ford <carsonf92@gmail.com>
2019-02-28 21:47:57 -0500

    UI programming

--- js/scripts.js
+++ js/scripts.js
@@ -17,16 +17,16 @@ var combinationLock = {
 	},
 	decrement: function(wheel) {
 		if (this.wheels[wheel] === 0) {
-			this.wheels[wheel] = 0;
+			this.wheels[wheel] = 9;
 		} else {
 			this.wheels[wheel]--;
 		}
 	},
 	check: function() {
 		if (this.combination === parseInt(this.wheels.join(''))) {
-			this.locked === false;
+			this.locked = false;
 		} else {
-			this.locked === true;
+			this.locked = true;
 		}
 	},
 	spin: function() {
@@ -51,4 +51,74 @@ function getRandomInt(min, max) {
 // ***
 // Presentation
 // ***
+
+// Increment buttons
+var increments = document.getElementsByClassName('increment');
+
+for (var i = 0; i < increments.length; i++) {
+    increments[i].addEventListener('click', function(){
+    	let wheelIndex = parseInt(this.getAttribute('index'));
+    	combinationLock.increment(wheelIndex);
+    	document.querySelectorAll('.digit')[wheelIndex].value = combinationLock.wheels[wheelIndex];
+    	checkLock();
+    });
+}
+
+// Decrement buttons
+var decrements = document.getElementsByClassName('decrement');
+
+for (var i = 0; i < decrements.length; i++) {
+    decrements[i].addEventListener('click', function(){
+    	let wheelIndex = parseInt(this.getAttribute('index'));
+    	combinationLock.decrement(wheelIndex);
+    	document.querySelectorAll('.digit')[wheelIndex].value = combinationLock.wheels[wheelIndex];
+    	checkLock();
+    });
+}
+
+// Keypress
+var wheels = document.getElementsByClassName('digit');
+
+for (var i = 0; i < wheels.length; i++) {
+	wheels[i].addEventListener('keyup', function(e){
+
+		// arrow key up
+		if (e.which === 38) {
+			let wheelIndex = parseInt(this.getAttribute('index'));
+			combinationLock.increment(wheelIndex);
+			document.querySelectorAll('.digit')[wheelIndex].value = combinationLock.wheels[wheelIndex];
+			checkLock();
+		}
+
+		// arrow key down
+		if (e.which === 40) {
+			let wheelIndex = parseInt(this.getAttribute('index'));
+			combinationLock.decrement(wheelIndex);
+			document.querySelectorAll('.digit')[wheelIndex].value = combinationLock.wheels[wheelIndex];
+			checkLock();
+		}
+
+		// number key (0 - 9)
+		if (e.which > 47 && e.which < 58 ) {
+			let wheelIndex = parseInt(this.getAttribute('index'));
+			combinationLock.wheels[wheelIndex] = parseInt(document.querySelectorAll('.digit')[wheelIndex].value);
+			checkLock();
+		}
+	});
+}
+
+// Check lock
+function checkLock() {
+	combinationLock.check();
+	if (combinationLock.locked === false) {
+		document.querySelector('#indicator').classList.remove('locked');
+		document.querySelector('#indicator').classList.add('unlocked');
+	} else {
+		document.querySelector('#indicator').classList.add('locked');
+		document.querySelector('#indicator').classList.remove('unlocked');
+	}
+}
+
+
+
 
